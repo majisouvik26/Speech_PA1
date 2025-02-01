f8cf9526c4415fb7d8fb54f8ab08e631d290514a
majisouvik26 <b22cs089@iitj.ac.in>
2025-02-01 14:39:56 +0530

    some fixes

--- classifier/train.py
+++ classifier/train.py
@@ -71,7 +71,7 @@ def main(args):
     model.to(device)
 
     criterion = nn.CrossEntropyLoss()
-    optimizer = optim.Adam(model.parameters(), lr=learning_rate, weight_decay=args.weight_decay)
+    optimizer = optim.AdamW(model.parameters(), lr=learning_rate, weight_decay=args.weight_decay)
     scheduler = optim.lr_scheduler.ReduceLROnPlateau(
         optimizer, mode='min', factor=args.lr_factor, patience=args.lr_patience, verbose=True
     )
@@ -111,7 +111,7 @@ if __name__ == "__main__":
     parser.add_argument('--epochs', type=int, default=20, help="Number of training epochs")
     parser.add_argument('--batch_size', type=int, default=32, help="Training batch size")
     parser.add_argument('--lr', type=float, default=1e-3, help="Learning rate")
-    parser.add_argument('--weight_decay', type=float, default=0, help="Weight decay for the optimizer")
+    parser.add_argument('--weight_decay', type=float, default=1e-5, help="Weight decay for the optimizer")
     parser.add_argument('--lr_factor', type=float, default=0.1, help="Factor by which the learning rate will be reduced")
     parser.add_argument('--lr_patience', type=int, default=10, help="Number of epochs with no improvement before reducing LR")
     parser.add_argument('--model_out', type=str, default="audio_cnn.pth", help="Path to save the trained model")
